e03296b754cc64a88c14252df4aa730ccfd5425b
diffficult <spaulo@gmail.com>
2023-10-09 13:41:00 -0300

    ⚙️ added config files and custom overrides to nvchad settings

--- .config/nvchad-custom/chadrc.lua
+++ .config/nvchad-custom/chadrc.lua
@@ -1,43 +1,109 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {
-  theme_toggle = { "tokyonight", "catppuccin" },
-  theme = 'tokyonight',
---- custom status line config 
-  statusline = {
-    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
-    -- default/round/block/arrow separators work only for default statusline theme
-    -- round and block will work for minimal theme only
-    separator_style = "arrow",
-    overriden_modules = nil,
+---@type ChadrcConfig
+local M = {}
+
+local core = require "custom.utils.core"
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+ theme = "nightowl",
+ theme_toggle = { "nightowl", "one_light" },
+ lsp_semantic_tokens = false,
+ statusline = core.statusline,
+ tabufline = core.tabufline,
+
+ cmp = {
+   icons = true,
+   lspkind_text = true,
+   style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+   border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+   selected_item_bg = "colored", -- colored / simple
+ },
+
+ lsp = {
+   signature = false,
+ },
+
+ telescope = { style = "bordered" },
+
+ extended_integrations = {
+   "dap",
+   "hop",
+   "rainbowdelimiters",
+   "codeactionmenu",
+   "todo",
+   "trouble",
+   "notify",
+ },
+
+ hl_override = highlights.override,
+ hl_add = highlights.add,
+
+--  nvdash = core.nvdash,
+
+-- nvdash (dashboard)
+ nvdash = {
+   load_on_startup = true,
+
+   header = {
+ 	[[                                                     ]],
+ 	[[  ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓ ]],
+ 	[[  ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒ ]],
+ 	[[ ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░ ]],
+ 	[[ ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██  ]],
+ 	[[ ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒ ]],
+ 	[[ ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░ ]],
+ 	[[ ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░ ]],
+  	[[    ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░    ]],
+ 	[[          ░    ░  ░    ░ ░        ░   ░         ░    ]],
+ 	[[                                 ░                   ]],
+  	[[                                                     ]],
+   },
+
+   buttons = {
+     { "  Find File", "Spc f f", "Telescope find_files" },
+     { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+     { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+     { "  Bookmarks", "Spc m a", "Telescope marks" },
+     { "  Themes", "Spc t h", "Telescope themes" },
+     { "  Mappings", "Spc c h", "NvCheatsheet" },
+   },
+ },
+
+}
+
+M.settings = {
+  cc_size = "130",
+  so_size = 10,
+
+  -- Blacklisted files where cc and so must be disabled
+  blacklist = {
+    "NvimTree",
+    "nvdash",
+    "nvcheatsheet",
+    "terminal",
+    "Trouble",
+    "help",
   },
-  -- nvdash (dashboard)
-  nvdash = {
-    load_on_startup = true,
+}
 
-    header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-    },
+M.lazy_nvim = core.lazy
 
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
-    },
+M.gitsigns = {
+  signs = {
+    add = { text = " " },
+    change = { text = " " },
+    delete = { text = " " },
+    topdelete = { text = " " },
+    changedelete = { text = " " },
+    untracked = { text = " " },
   },
- }
+}
 
-M.plugins = 'custom.plugins'
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
 
 return M
